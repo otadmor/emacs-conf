1430d2cfef7a7fb2f799baac33df1039e2a5df4a
otadmor <otadmor@gmail.com>
2019-02-15 04:00:44 +0200

    keep scratch alive

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -103,15 +103,27 @@
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 (setq scroll-step 1) ;; keyboard scroll one line at a time
 
-; display stuff
-(save-excursion
-  (switch-to-buffer "*scratch*")
-  (insert ";;")
-  (newline)
-  (insert ";; Welcome h4x0r !!!")
-  (newline)
-  (insert ";;")
-  (newline))
+;; display stuff
+(defun init-scratch()
+  (save-excursion
+    (switch-to-buffer "*scratch*")
+    (setf (buffer-string) "")
+    (insert ";;")
+    (newline)
+    (insert ";; Welcome h4x0r !!!")
+    (newline)
+    (insert ";;")
+    (newline)))
+(init-scratch)
+
+(add-hook 'kill-buffer-query-functions #'dont-kill-scratch)
+(defun dont-kill-scratch ()
+  (if (not (equal (buffer-name) "*scratch*"))
+      t
+    (message "Not allowed to kill %s, burying instead" (buffer-name))
+    (init-scratch)
+    (bury-buffer)
+    nil))
 
 (setenv "PAGER" "cat")
 
