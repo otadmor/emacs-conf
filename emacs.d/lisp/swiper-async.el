a1974b3b6e59ca031eddcdcdb78f615f3ba12d8c
otadmor <otadmor@gmail.com>
2019-03-16 20:14:13 +0200

    [swiper-async] added deletion of lines and lines insertions support. fails when trying to delete the last lines.

--- emacs.d/lisp/swiper-async.el
+++ emacs.d/lisp/swiper-async.el
@@ -30,18 +30,46 @@
   :type 'integer)
 
 (defun swiper--get-line (item)
+  ; (message "item %S" item)
   (get-text-property 0 'swiper-line-number item))
+(defun swiper--get-begin (item)
+  (get-text-property 0 'begin item))
+(defun swiper--get-end (item)
+  (get-text-property 0 'end item))
+
+
+(defun swiper--line-with-borders ()
+  (let* ((beg (cond ((and (eq major-mode 'dired-mode)
+                          (bound-and-true-p dired-isearch-filenames))
+                     (dired-move-to-filename)
+                     (point))
+                    (swiper-use-visual-line
+                     (save-excursion
+                       (beginning-of-visual-line)
+                       (point)))
+                    (t
+                     (point))))
+         (end (if swiper-use-visual-line
+                  (save-excursion
+                    (end-of-visual-line)
+                    (point))
+                (line-end-position))))
+    (let (
+          (str (concat " " (buffer-substring beg end)))
+          )
+      (put-text-property 0 1 'begin beg str)
+      (put-text-property 0 1 'end end str)
+      str)))
+(defalias 'swiper--line 'swiper--line-with-borders)
 
 (defun swiper--async-filter (buffer begin end deleted-length)
   "Receive from buffer the output STR.
 Update the minibuffer with the amount of lines collected every
 `swiper-async-filter-update-time' microseconds since the last update."
-  (when (or (and (= deleted-length 0)
-                 (= (line-end-position end) (point-max))
-                                        ; (time-less-p (list 0 0 swiper-async-filter-update-time)
-                                        ; (time-since counsel--async-time))
-                 )
-            (= (line-end-position end) (line-beginning-position begin)))
+  (when (and t
+             ;; (time-less-p (list 0 0 swiper-async-filter-update-time)
+             ;; (time-since counsel--async-time))
+             )
     (setq ivy--old-cands ivy--all-candidates)
     (let (
           (new-candidates (with-current-buffer buffer (swiper--async-candidates begin end)))
@@ -53,11 +81,39 @@ Update the minibuffer with the amount of lines collected every
             (i 0)
             (first-index)
             (last-index)
+            (deleted-lines 0)
+            (deleted-leftover deleted-length)
+            (lines-diff 0)
             )
+        ; (message "first %S" (swiper--get-line first-new-candidate))
+        ; (message "last %S" (swiper--get-line last-new-candidate))
         (cl-loop for cand in ivy--all-candidates do
                  (when (string= (swiper--get-line cand)
                                 (swiper--get-line first-new-candidate))
                    (setq first-index i))
+                 (when (and (not (null first-index))
+                            (> deleted-leftover 0))
+                   (let (
+                         (cand-begin (swiper--get-begin cand))
+                         (cand-end (swiper--get-end cand))
+                         )
+                     (let (
+                           (begin-reference (if (and (> begin cand-begin)
+                                                     (< begin cand-end))
+                                                begin
+                                              cand-begin))
+                           )
+                       (let (
+                             (drained-length (min deleted-leftover
+                                                  (- cand-end begin-reference)))
+                             )
+                         (cl-incf deleted-lines)
+                         (setq deleted-leftover (- deleted-leftover
+                                                   drained-length))
+                         )
+                       )
+                     )
+                   )
                  (when (string= (swiper--get-line cand)
                                 (swiper--get-line last-new-candidate))
                    (setq last-index i))
@@ -66,6 +122,12 @@ Update the minibuffer with the amount of lines collected every
           (setq first-index (length ivy--all-candidates)))
         (when (null last-index)
           (setq last-index (length ivy--all-candidates)))
+        ; (message "deleted lines %S" deleted-lines)
+        ; (message "added lines %S" (- last-index first-index))
+        (setq lines-diff (- (- last-index first-index) deleted-lines))
+        (when (> deleted-lines 0)
+          (setq deleted-lines (- deleted-lines 1)))
+        (setq last-index (+ last-index deleted-lines))
         (if (> first-index 0)
             (let (
                   (cand-firsts (nthcdr (- first-index 1) ivy--all-candidates))
@@ -79,7 +141,8 @@ Update the minibuffer with the amount of lines collected every
                 (cand-lasts (nthcdr last-index ivy--all-candidates))
                 )
             (setcdr (last new-candidates) cand-lasts)
-            (setq ivy--all-candidates new-candidates))))
+            (setq ivy--all-candidates new-candidates)))
+        (setq ivy--index (+ ivy--index lines-diff)))
       (ivy--set-candidates ivy--all-candidates))
     (setq ivy--old-cands ivy--all-candidates)
     (let (
