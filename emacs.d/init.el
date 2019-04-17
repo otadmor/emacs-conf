03fca93b97f8c5f01e14f35c65245d041250fce1
otadmor <otadmor@gmail.com>
2019-04-17 04:22:59 +0300

    partialy fix future stack not saved.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1366,19 +1366,20 @@ Ignore PROJ"
 
 (advice-add 'ivy-read :around #'wrap-winstack-hook)
 (advice-add 'switch-to-buffer :around #'wrap-winstack-hook)
-(advice-add 'other-window :around #'wrap-winstack-hook)
-(advice-add 'select-window :around #'wrap-winstack-hook)
-(advice-add 'quit-window :around #'wrap-winstack-hook)
-(advice-add 'windmove-left :around #'wrap-winstack-hook)
-(advice-add 'windmove-right :around #'wrap-winstack-hook)
-(advice-add 'windmove-up :around #'wrap-winstack-hook)
-(advice-add 'windmove-down :around #'wrap-winstack-hook)
+; (advice-add 'other-window :around #'wrap-winstack-hook)
+; (advice-add 'select-window :around #'wrap-winstack-hook)
+; (advice-add 'quit-window :around #'wrap-winstack-hook)
+; (advice-add 'windmove-left :around #'wrap-winstack-hook)
+; (advice-add 'windmove-right :around #'wrap-winstack-hook)
+; (advice-add 'windmove-up :around #'wrap-winstack-hook)
+; (advice-add 'windmove-down :around #'wrap-winstack-hook)
 (advice-add 'goto-line :around #'wrap-winstack-hook)
 ; (advice-add 'goto-char :around #'wrap-winstack-hook)
 
 (advice-add 'dumb-jump-goto-file-line :around #'wrap-winstack-hook)
 (advice-add 'jedi:goto-definition--nth :around #'wrap-winstack-hook)
 
+(advice-add 'persp-asave-on-exit :around #'wrap-winstack-command-hook)
 	
 (which-function-mode 1)
 
