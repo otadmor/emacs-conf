59163557e31c74bc6a6ab181abb2ba12e8de942e
otadmor <otadmor@gmail.com>
2019-04-24 21:14:47 +0300

    extracted ivy-utils code

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -312,42 +312,7 @@
 
 ;(setq ivy-magic-slash-non-match-action nil)
 
-; (with-current-buffer buffer
-;   )
-; (ivy-exit-with-action
-;  (lambda (_) (pop-to-buffer buffer)))
-
-(defun ivy-shell ()
-  (interactive)
-  (if ivy--directory
-    (ivy-quit-and-run
-        (switch-to-buffer (old-shell-with-dir (expand-file-name ivy--directory))))
-    (user-error
-     "Not completing files currently")))
-
-(defun ivy-new-shell ()
-  (interactive)
-  (if ivy--directory
-      (ivy-quit-and-run
-        (switch-to-buffer (new-shell-with-dir (expand-file-name ivy--directory))))
-    (user-error
-     "Not completing files currently")))
-
-(defun ivy-magit-status ()
-  (interactive)
-  (if ivy--directory
-      (ivy-quit-and-run
-       (magit-status ivy--directory))
-    (user-error
-     "Not completing files currently")))
-
-(defun ivy-python ()
-  (interactive)
-  (if ivy--directory
-      (ivy-quit-and-run
-       (new-python-in-dir ivy--directory))
-    (user-error
-     "Not completing files currently")))
+(require 'ivy-utils)
 
 (require 'swiper)
 (define-key ivy-minibuffer-map (kbd "C-d") #'ivy-immediate-done)
