b32c7b37826de8dfd847e80530fe11e57531c900
otadmor <otadmor@gmail.com>
2019-04-24 20:55:56 +0300

    [counsel-ag] extracted formating and preselect code.

---
+++ emacs.d/lisp/counsel-ag-ext.el
@@ -0,0 +1,72 @@
+(require 'ivy)
+; (push '(counsel-ag . ivy-recompute-index-swiper-async) ivy-index-functions-alist)
+
+(defun ag--format-result (proj result)
+  (let (
+        (rep-path (plist-get result :path))
+        )
+    (format "%s:%s:%s"
+            (if (null rep-path) proj (s-replace proj "" (plist-get result :path)))
+            (plist-get result :line)
+            (plist-get result :context))))
+
+
+(defun preselect-line(proj)
+  (with-selected-window (if (active-minibuffer-window)
+                            (if (eq (active-minibuffer-window) (frame-selected-window))
+                                (get-mru-window)
+                              (frame-selected-window))
+                          (frame-selected-window))
+    (let (
+          (pselect-record (list :path buffer-file-name
+                                :line (string-to-number (format-mode-line "%l"))
+                                :context (replace-regexp-in-string "\n$" "" (thing-at-point 'line t))))
+          )
+      (ag--format-result (expand-file-name proj) pselect-record))))
+
+(defun counsel-ag-preselect (&optional initial-input initial-directory extra-ag-args ag-prompt)
+  "Grep for a string in the current directory using ag.
+INITIAL-INPUT can be given as the initial minibuffer input.
+INITIAL-DIRECTORY, if non-nil, is used as the root directory for search.
+EXTRA-AG-ARGS string, if non-nil, is appended to `counsel-ag-base-command'.
+AG-PROMPT, if non-nil, is passed as `ivy-read' prompt argument."
+  (interactive)
+  (setq counsel-ag-command counsel-ag-base-command)
+  (counsel-require-program (car (split-string counsel-ag-command)))
+  (when current-prefix-arg
+    (setq initial-directory
+          (or initial-directory
+              (read-directory-name (concat
+                                    (car (split-string counsel-ag-command))
+                                    " in directory: "))))
+    (setq extra-ag-args
+          (or extra-ag-args
+              (read-from-minibuffer (format
+                                     "%s args: "
+                                     (car (split-string counsel-ag-command)))))))
+  (setq counsel-ag-command (counsel--format-ag-command (or extra-ag-args "") "%s"))
+  (let ((default-directory (or initial-directory
+                               (locate-dominating-file default-directory ".git")
+                               default-directory)))
+    (let (
+          (preselect (preselect-line default-directory))
+          )
+      (ivy-read (or ag-prompt
+                    (concat (car (split-string counsel-ag-command)) ": "))
+                #'counsel-ag-function
+                :initial-input initial-input
+                :preselect preselect
+                :dynamic-collection t
+                :keymap counsel-ag-map
+                :history 'counsel-git-grep-history
+                :action #'counsel-git-grep-action
+                :unwind (lambda ()
+                          (counsel-delete-process)
+                          (swiper--cleanup))
+                :caller 'counsel-ag))))
+
+(defun counselag-which-func-update (x)
+  (swiper--async-which-func-update))
+(advice-add 'counsel-git-grep-action :after #'counselag-which-func-update)
+
+(provide 'counsel-ag-ext)
