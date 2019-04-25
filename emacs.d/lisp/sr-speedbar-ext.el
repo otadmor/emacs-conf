c0e9da8023c9eb6190f2b67d74d18fcaad248bb7
otadmor <otadmor@gmail.com>
2019-04-25 14:28:20 +0300

    created sr-speedbar-ext module

---
+++ emacs.d/lisp/sr-speedbar-ext.el
@@ -0,0 +1,86 @@
+(setq helm-alive-p nil) ; fix sr-speedbur bug
+(require 'sr-speedbar)
+(sr-speedbar-refresh-turn-off)
+(defun sr-speedbar-find-window-buffer()
+  (setq sr-speedbar-window (get-buffer-window sr-speedbar-buffer-name 'visible))
+  (if sr-speedbar-window
+      (setq speedbar-buffer (window-buffer sr-speedbar-window))
+      (setq speedbar-buffer nil)))
+
+(setq sr-speedbar-right-side t)
+
+(defun sr-speedbar-get-window ()
+  "Get `sr-speedbar' window."
+  (let (
+        (current-window (frame-root-window))
+        )
+    (let (
+          ;; Get split new window.
+          (new-window (split-window
+                       current-window
+                       (if sr-speedbar-right-side
+                           (- (sr-speedbar-current-window-take-width current-window) sr-speedbar-width)
+                         sr-speedbar-width)
+                       t))
+          )
+    ;; Select split window.
+      (setq sr-speedbar-window
+            (if sr-speedbar-right-side
+                ;; Select right window when `sr-speedbar-right-side' is enable.
+                new-window
+              ;; Otherwise select left widnow.
+              current-window)))))
+
+(defun sr-speedbar-exist-p-hook(orig-fun &rest args)
+  (sr-speedbar-find-window-buffer)
+  (apply orig-fun args)
+  ;(get-buffer-window sr-speedbar-buffer-name 'visible)
+  )
+(advice-add 'sr-speedbar-exist-p :around #'sr-speedbar-exist-p-hook)
+
+(defun sr-speedbar-navigate() (interactive)
+  (let (
+        (root-dir (read-directory-name "Navigate Speedbar: "))
+        )
+    (with-temp-buffer
+      (setq default-directory root-dir)
+      (speedbar-refresh))
+    root-dir))
+
+(add-hook 'speedbar-reconfigure-keymaps-hook
+          '(lambda ()
+             (define-key speedbar-mode-map (kbd "<backspace>") 'speedbar-up-directory)
+             (define-key speedbar-mode-map [right] 'speedbar-flush-expand-line)
+             (define-key speedbar-mode-map [left] 'speedbar-contract-line)
+             (define-key speedbar-mode-map [M-up] 'speedbar-restricted-prev)
+             (define-key speedbar-mode-map [M-down] 'speedbar-restricted-next)
+             (define-key speedbar-mode-map [up] 'speedbar-prev)
+             (define-key speedbar-mode-map [down] 'speedbar-next)
+             (define-key speedbar-mode-map (kbd "M-g") 'sr-speedbar-navigate)
+             (define-key speedbar-mode-map [(control meta p)] 'winstack-pop)
+             (define-key speedbar-mode-map [(control meta n)] 'winstack-next)
+             ))
+
+(setq speedbar-last-window nil)
+(defun sr-speedbar-toggle-keep-window ()
+  "Toggle sr-speedbar window.
+Toggle visibility of sr-speedbar by resizing
+the `sr-speedbar-window' to a minimal width
+or the last width when visible.
+Use this function to create or toggle visibility
+of a speedbar-window.  It will be created if necessary."
+  (interactive)
+  (if (sr-speedbar-exist-p)
+      (progn
+        (sr-speedbar-close)
+        (when
+            (and speedbar-last-window (window-live-p speedbar-last-window))
+          (select-window speedbar-last-window))
+        (setq speedbar-last-window nil))
+    (progn
+      (sr-speedbar-open)
+      (setq speedbar-last-window (frame-selected-window))
+      (sr-speedbar-select-window))))
+(global-set-key (kbd "C-e") 'sr-speedbar-toggle-keep-window)
+
+(provide 'sr-speedbar-ext)
