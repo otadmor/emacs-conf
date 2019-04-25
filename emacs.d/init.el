cdcf5becfddb464d8fd6efd21746c3d444002bf0
otadmor <otadmor@gmail.com>
2019-04-25 14:43:28 +0300

    added ivy-dump-jump

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -470,82 +470,8 @@
 
 (require 'persp-mode-ext)
 
-(require 'dumb-jump)
+(require 'ivy-dump-jump)
 (global-set-key goto-def-key 'dumb-jump-go)
-
-(defun dumb-jump-ivy-minibuffer-keyboard-quit () (interactive)
-  (with-ivy-window
-    (goto-char dumb-jump--opoint))
-  (minibuffer-keyboard-quit))
-
-(defun dumb-jump--goto-original-point()
-  (interactive)
-  (with-ivy-window
-    (setq swiper--current-match-start dumb-jump--opoint)
-    (setq swiper--current-line (string-to-number (format-mode-line "%l")))
-    (goto-char dumb-jump--opoint)
-    ))
-
-(setq dumb-jump-last-query nil)
-(defalias 'dumb-jump-populate-regexes (lambda (look-for regexes variant)
-  "Take list of REGEXES and populate the LOOK-FOR target and return that list."
-  (setq dumb-jump-last-query look-for)
-  (--map (dumb-jump-populate-regex it look-for variant) regexes)))
-
-(defvar dumb-jump-ivy-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "<down>") (lambda() (interactive) (ivy-next-line-and-call)))
-    (define-key map (kbd "<up>") (lambda() (interactive) (ivy-previous-line-and-call)))
-    (define-key map (kbd "C-<up>") 'ivy-previous-line)
-    (define-key map (kbd "C-<down>") 'ivy-next-line)
-    (define-key map (kbd "M-f")
-      (lambda () (interactive)
-        (ivy-quit-and-run
-          (counsel-ag-preselect dumb-jump-last-query)
-          )))
-    (define-key map (kbd "C-l") 'ivy-call-and-recenter)
-    (define-key map (kbd "M-C-p") 'dumb-jump--goto-original-point)
-    (define-key map (kbd "M-C-n") (lambda () (interactive)))
-    (define-key map (kbd "C-g") 'dumb-jump-ivy-minibuffer-keyboard-quit)
-    map))
-
-(ivy-set-display-transformer 'dumb-jump-ivy-jump-to-selected 'counsel-git-grep-transformer)
-
-(add-hook 'dumb-jump-after-jump-hook 'swiper--async-which-func-update)
-(defun dumb-jump-ivy-jump-to-selected-with-call (results choices proj)
-  "Offer CHOICES as canidates through ivy-read then execute
-dumb-jump-to-selected on RESULTS CHOICES and selected choice.
-Ignore PROJ"
-  (setq dumb-jump--opoint (point))
-  (let (
-        (pselect-record (list :path (file-name-nondirectory buffer-file-name)
-                              :line (string-to-number (format-mode-line "%l"))
-                              :context (thing-at-point 'line t)))
-        )
-      (unwind-protect
-          (let (
-                (res (ivy-read "Jump to: " choices
-                               :preselect (dumb-jump--format-result proj pselect-record)
-                               :keymap dumb-jump-ivy-map
-                               ;;:unwind #'swiper--cleanup
-                               :caller 'dumb-jump-ivy-jump-to-selected
-                               :action (lambda (x)
-                                         (dumb-jump-to-selected results choices x))
-                               ;;   :require-match t
-                               :history 'swiper-history
-                               ))
-                )
-            (unless res
-              (goto-char dumb-jump--opoint))
-            (unless (or res (string= ivy-text ""))
-              (cl-pushnew ivy-text swiper-history))
-                                        ; (when swiper--reveal-mode
-                                        ; (reveal-mode 1))
-            ))))
-
-(defalias 'dumb-jump-ivy-jump-to-selected 'dumb-jump-ivy-jump-to-selected-with-call)
-
-(setq dumb-jump-selector 'ivy)
 
 
 ; (advice-add 'ivy-read :around #'wrap-winstack-hook)
