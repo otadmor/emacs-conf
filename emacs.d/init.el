c1e38c482297e8a91727eb1c693e8fcc89f9219d
otadmor <otadmor@gmail.com>
2019-05-25 19:18:01 +0300

    [ivy-rich] fixed nil error when no summary and not symbol data available.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -87,12 +87,30 @@
 (ivy-mode t)
 
 (require 'ivy-rich)
+(defun popup-item-summary-or-empty(item)
+  (if (null item)
+      ""
+    (let (
+          (data (popup-item-summary item))
+          )
+      (if (null data)
+          ""
+        data))))
+(defun popup-item-symbol-or-empty(item)
+  (if (null item)
+      ""
+    (let (
+          (data (popup-item-symbol item))
+          )
+      (if (null data)
+          ""
+        data))))
 (add-to-list
  'ivy-rich-display-transformers-list
  '(:columns
    ((ivy-cleanup-string (:width 30))
-    (popup-item-summary (:width 30 :face font-lock-doc-face))
-    (popup-item-symbol (:face font-lock-comment-face)))))
+    (popup-item-summary-or-empty (:width 30 :face font-lock-doc-face))
+    (popup-item-symbol-or-empty (:face font-lock-comment-face)))))
 (add-to-list
  'ivy-rich-display-transformers-list
  'ivy-completion-in-region)
