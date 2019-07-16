890066810d3f6b91c59f2fa5b694b31f82a90a73
otadmor <otadmor@gmail.com>
2019-07-16 23:29:21 +0300

    [comint-ext] dont delete prompt on backspace.

--- emacs.d/lisp/comint-ext.el
+++ emacs.d/lisp/comint-ext.el
@@ -13,4 +13,10 @@
       (concat comint-password-prompt-regexp
               "\\|^.*\\\\' password:\\s *\\'"))
 
+(defun comint-delete-backward-char(fun &rest args)
+  (when (or (not (consp comint-last-prompt))
+            (/= (cdr comint-last-prompt) (point)))
+    (apply fun args)))
+(advice-add 'delete-backward-char :around #'comint-delete-backward-char)
+
 (provide 'comint-ext)
