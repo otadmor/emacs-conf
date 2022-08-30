1902b515e75472f1b233b714b5a31db31e7a852d
otadmor <otadmor@gmail.com>
2022-08-30 19:44:22 +0300

    GDBCOMPILE added compilation for linux arm and fixed android compilations

--- compile_gdb_gdbserver_12.1.sh
+++ compile_gdb_gdbserver_12.1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # https://dl.google.com/android/repository/android-ndk-r25b-linux.zip
-#NDK=/opt/android-ndk-r25b/
-NDK=/home/ubuntu/android/ndk/android-ndk-r25b/
+NDK=/opt/android-ndk-r25b/
 if [[ ! -d $NDK ]]; then
     echo Missing ndk $NDK
     exit 1
@@ -76,19 +75,22 @@ export PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH
 mkdir zlib && cd zlib && ../../zlib/configure  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=aarch64-linux-android --program-prefix=aarch64-linux-android- \
-        LDFLAGS=-static CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=aarch64-linux-android21-clang CXX=aarch64-linux-android21-clang++ && \
     make && cd .. || exit 1
 mkdir bfd && cd bfd && ../../bfd/configure  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=aarch64-linux-android --program-prefix=aarch64-linux-android- \
-        LDFLAGS=-static CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=aarch64-linux-android21-clang CXX=aarch64-linux-android21-clang++ && \
     make && cd .. || exit 1
 chmod +x ../gnulib/configure && mkdir gnulib && cd gnulib && ../../gnulib/configure  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=aarch64-linux-android --program-prefix=aarch64-linux-android- \
-        LDFLAGS=-static CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=aarch64-linux-android21-clang CXX=aarch64-linux-android21-clang++ && \
     sed -i 's/$(srcdir)\/$(GNULIB_PARENT_DIR)\//..\/$(GNULIB_PARENT_DIR)\//g' Makefile.gnulib.inc && \
     sed -i 's/HAVE_MKOSTEMP = 1/HAVE_MKOSTEMP = 0/g' import/Makefile && \
@@ -97,19 +99,22 @@ chmod +x ../gnulib/configure && mkdir gnulib && cd gnulib && ../../gnulib/config
 mkdir gdbsupport && cd gdbsupport && ../../gdbsupport/configure  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=aarch64-linux-android --program-prefix=aarch64-linux-android- \
-        LDFLAGS=-static CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=aarch64-linux-android21-clang CXX=aarch64-linux-android21-clang++ && \
     make && cd .. || exit 1
 mkdir libiberty && cd libiberty && ../../libiberty/configure \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=aarch64-linux-android --program-prefix=aarch64-linux-android- \
-        LDFLAGS=-static CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=aarch64-linux-android21-clang CXX=aarch64-linux-android21-clang++ && \
     make && cd .. || exit 1
 PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH ../gdbserver/configure CPPFLAGS="-include gnulib/import/stdio.h"  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=aarch64-linux-android --program-prefix=aarch64-linux-android- \
-        LDFLAGS=-static CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=aarch64-linux-android21-clang CXX=aarch64-linux-android21-clang++ && \
     sed -i 's/-Wl,--dynamic-list=$(srcdir)\/proc-service.list//g' Makefile && \
     sed -i 's/GNULIB_BUILDDIR = ..\/gnulib/GNULIB_BUILDDIR = gnulib/g' Makefile && \
@@ -124,19 +129,22 @@ export PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH
 mkdir zlib && cd zlib && ../../zlib/configure  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=arm-linux-androideabi --program-prefix=arm-linux-androideabi- \
-        LDFLAGS="-static" CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=armv7a-linux-androideabi21-clang CXX=armv7a-linux-androideabi21-clang++ && \
     make && cd .. || exit 1
 mkdir bfd && cd bfd && ../../bfd/configure  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=arm-linux-androideabi --program-prefix=arm-linux-androideabi- \
-        LDFLAGS="-static" CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=armv7a-linux-androideabi21-clang CXX=armv7a-linux-androideabi21-clang++ && \
     make && cd .. || exit 1
 chmod +x ../gnulib/configure && mkdir gnulib && cd gnulib && ../../gnulib/configure  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=arm-linux-androideabi --program-prefix=arm-linux-androideabi- \
-        LDFLAGS="-static" CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=armv7a-linux-androideabi21-clang CXX=armv7a-linux-androideabi21-clang++ && \
     sed -i 's/$(srcdir)\/$(GNULIB_PARENT_DIR)\//..\/$(GNULIB_PARENT_DIR)\//g' Makefile.gnulib.inc && \
     sed -i 's/HAVE_MKOSTEMP = 1/HAVE_MKOSTEMP = 0/g' import/Makefile && \
@@ -145,19 +153,22 @@ chmod +x ../gnulib/configure && mkdir gnulib && cd gnulib && ../../gnulib/config
 mkdir gdbsupport && cd gdbsupport && ../../gdbsupport/configure  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=arm-linux-androideabi --program-prefix=arm-linux-androideabi- \
-        LDFLAGS="-static" CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=armv7a-linux-androideabi21-clang CXX=armv7a-linux-androideabi21-clang++ && \
     make && cd .. || exit 1
 mkdir libiberty && cd libiberty && ../../libiberty/configure \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=arm-linux-androideabi --program-prefix=arm-linux-androideabi- \
-        LDFLAGS="-static" CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=armv7a-linux-androideabi21-clang CXX=armv7a-linux-androideabi21-clang++ && \
     make && cd .. || exit 1
 PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH ../gdbserver/configure CPPFLAGS="-include gnulib/import/stdio.h"  \
         PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH \
         --host=arm-linux-androideabi --program-prefix=arm-linux-androideabi- \
-        LDFLAGS="-static" CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
+        LDFLAGS="-static -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+        CPPFLAGS="-DHAVE_ELF32_AUXV_T -DHAVE_ELF64_AUXV_T" \
         CC=armv7a-linux-androideabi21-clang CXX=armv7a-linux-androideabi21-clang++ && \
     sed -i 's/-Wl,--dynamic-list=$(srcdir)\/proc-service.list//g' Makefile && \
     sed -i 's/GNULIB_BUILDDIR = ..\/gnulib/GNULIB_BUILDDIR = gnulib/g' Makefile && \
@@ -165,3 +176,36 @@ PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH ../gdbserver/configur
     sed -i 's/LIBIBERTY_BUILDDIR = ..\/libiberty/LIBIBERTY_BUILDDIR = libiberty/g' Makefile && \
     sed -i 's/GDBSUPPORT_BUILDDIR = ..\/gdbsupport/GDBSUPPORT_BUILDDIR = gdbsupport/g' Makefile && \
     make gdbserver && llvm-strip gdbserver && cd .. || exit 1
+
+sudo apt-get install gcc-arm-linux-gnueabi g++-arm-linux-gnueabi g++-11-arm-linux-gnueabi gcc-11-arm-linux-gnueabi || exit 1
+
+mkdir build-gdbserver-arm-linux-gnueabi && cd build-gdbserver-arm-linux-gnueabi || exit 1
+mkdir zlib && cd zlib && ../../zlib/configure  \
+        LDFLAGS=-static --host=arm-linux-gnueabi \
+        CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ AR=arm-linux-gnueabi-ar && \
+    make && cd .. || exit 1
+mkdir bfd && cd bfd && ../../bfd/configure  \
+        --host=arm-linux-gnueabi \
+        CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ AR=arm-linux-gnueabi-ar && \
+    make && cd .. || exit 1
+chmod +x ../gnulib/configure && mkdir gnulib && cd gnulib && ../../gnulib/configure  \
+        LDFLAGS=-static --host=arm-linux-gnueabi \
+        CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ AR=arm-linux-gnueabi-ar && \
+    sed -i 's/$(srcdir)\/$(GNULIB_PARENT_DIR)\//..\/$(GNULIB_PARENT_DIR)\//g' Makefile.gnulib.inc && \
+    make && cd .. || exit 1
+mkdir gdbsupport && cd gdbsupport && ../../gdbsupport/configure  \
+        LDFLAGS=-static --host=arm-linux-gnueabi \
+        CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ AR=arm-linux-gnueabi-ar && \
+    make && cd .. || exit 1
+mkdir libiberty && cd libiberty && ../../libiberty/configure \
+        LDFLAGS=-static --host=arm-linux-gnueabi \
+        CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ AR=arm-linux-gnueabi-ar && \
+    make && cd .. || exit 1
+../gdbserver/configure CPPFLAGS="-include gnulib/import/stdio.h"  \
+        LDFLAGS=-static --host=arm-linux-gnueabi \
+        CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ AR=arm-linux-gnueabi-ar && \
+    sed -i 's/GNULIB_BUILDDIR = ..\/gnulib/GNULIB_BUILDDIR = gnulib/g' Makefile && \
+    sed -i 's/GNULIB_PARENT_DIR = ../GNULIB_PARENT_DIR = ./g' Makefile && \
+    sed -i 's/LIBIBERTY_BUILDDIR = ..\/libiberty/LIBIBERTY_BUILDDIR = libiberty/g' Makefile && \
+    sed -i 's/GDBSUPPORT_BUILDDIR = ..\/gdbsupport/GDBSUPPORT_BUILDDIR = gdbsupport/g' Makefile && \
+    make gdbserver && arm-linux-gnueabi-strip gdbserver && cd .. || exit 1
